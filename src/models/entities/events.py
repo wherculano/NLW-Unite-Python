dfa876f7c4791c42c8d81e4e34d66e8049dc34f9
Wagner Herculano <wagherculano@hotmail.com>
2024-08-04 22:35:12 -0300

    Added events

---
+++ src/models/entities/events.py
@@ -0,0 +1,13 @@
+from src.models.settings.base import Base
+from sqlalchemy import Column, Integer, String
+
+
+class Events(Base):
+    __tablename__ = "events"
+
+    id = Column(String, primary_key=True)
+    title = Column(String, nullable=False)
+    details = Column(String)
+    slug = Column(String, nullable=False)
+    maximum_attendees = Column(Integer)
+    
