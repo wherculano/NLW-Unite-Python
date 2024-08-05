d61b2bc1c757532970b7affe48a0f6fcbe0ca99b
Wagner Herculano <wagherculano@hotmail.com>
2024-08-05 00:08:48 -0300

    Added class representation

--- src/models/entities/events.py
+++ src/models/entities/events.py
@@ -10,4 +10,6 @@ class Events(Base):
     details = Column(String)
     slug = Column(String, nullable=False)
     maximum_attendees = Column(Integer)
-    
+
+    def __repr__(self):
+        return f"Events(id={self.id}, title={self.title}, details={self.details}, slug={self.slug}, maximum_attendees={self.maximum_attendees})"
