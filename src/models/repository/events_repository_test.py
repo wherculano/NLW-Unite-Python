84fd205312cc876a85cfb8b0e063ae27260a6a45
Wagner Herculano <wagherculano@hotmail.com>
2024-08-06 22:18:13 -0300

    Refactored test

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -1,31 +1,33 @@
+import pytest
 from uuid import uuid4
+
 from src.models.repository.events_repository import EventRepository
 from src.models.settings.connection import db_connection_handler
-from sqlalchemy.exc import IntegrityError, NoResultFound
 
 
 db_connection_handler.connect_to_db()
 
 ID_TEST = str(uuid4())
 
+
+@pytest.mark.skip(reason="Dont want to create a new event.")
 def test_inser_event():
     event = {
         "uuid": ID_TEST,
-        "title": "Evento Teste",
-        "details": "Testando insercao de dados",
-        "slug": f"evento-teste-{ID_TEST[:8]}",
+        "title": "Event test",
+        "details": "Test inserting data.",
+        "slug": f"event-test-{ID_TEST[:8]}",
         "maximum_attendees": 10
     }
     event_repository = EventRepository()
     response = event_repository.insert_event(event)
-    print(f"Dados inseridos com sucesso: {response}")
+    print(f"Successfully created: {response}")
 
+
+@pytest.mark.skip(reason="There is no event with a new ID.")
 def test_get_event_by_id():
     db_connection_handler.connect_to_db()
     event_id = ID_TEST
     event_repository = EventRepository()
-    try:
-        response = event_repository.get_event_by_id(event_id)
-        print(response)
-    except NoResultFound:
-        print("Nenhum dado encontrado!")
+    response = event_repository.get_event_by_id(event_id)
+    print(response)
