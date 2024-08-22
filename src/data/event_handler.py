b6f22bba508381c7d553bdefe4a6145b93c00392
Wagner Herculano <wagherculano@hotmail.com>
2024-08-21 21:37:55 -0300

    Added function find_by_id

--- src/data/event_handler.py
+++ src/data/event_handler.py
@@ -16,3 +16,28 @@ class EventHandler:
             body={"eventId": body["uuid"]},
             status_code=201
         )
+
+    def find_by_id(self, http_request: HttpRequest) -> HttpResponse:
+        event_id = http_request.param["event_id"]
+        event = self.__events_repository.get_event_by_id(event_id)
+        if not event:
+            return HttpResponse(
+                body={"message": "Event not found"},
+                status_code=404
+            )
+
+        event_count_attendees = self.__events_repository.count_event_attendees(
+            event_id)
+        return HttpResponse(
+            body={
+                "event": {
+                    "id": event.id,
+                    "title": event.title,
+                    "details": event.details,
+                    "slug": event.slug,
+                    "maximumAttendees": event.maximum_attendees,
+                    "attendeesAmount": event_count_attendees["attendeesAmount"]
+                }
+            },
+            status_code=200
+        )
